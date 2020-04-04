11b6f3ff86fb5dbe17244cf7f88b1b4e9880e1bd
სანდრო <kopalianisandro2@gmail.com>
2020-04-04 17:33:13 +0400

    Html

---
+++ Untitled-2.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+	<title>Document</title>
+	<style>
+		* {
+			margin: 0;
+		}
+
+        .h3 {
+			background-color: blue;
+		}
+	</style>
+</head>
+
+<body>
+	<?php
+	$conn = new PDO('mysql:host=64.225.79.16;dbname=kopaliani', 'kopaliani', 'mziuri2020');
+	?>
+
+	<nav>
+		<ul class="h3">
+			<h3>Blog Sports</h3>
+		</ul>
+
+		<img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/937B4627-FF01-4674-BE07-95E911D87627.png" width="1500px" height="700px" alt="Error">
+
+	</nav>
+
+</body>
+
+</html>
